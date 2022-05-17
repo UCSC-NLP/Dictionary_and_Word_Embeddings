ed602d352d70c8b9dcebefe109bcd2ca7e1cf34e
AceBose <architbose99@gmail.com>
2022-05-17 14:36:43 -0700

    Adding code to extract FastText Embeddings -- Harded coded filepaths

--- Readme.md
+++ Readme.md
@@ -13,3 +13,32 @@ Instructions:
 Guides to git workflow:
 
 1. https://gist.github.com/Chaser324/ce0505fbed06b947d962
+
+
+For words that were not found in Glove, 0 vector of same size was utilized
+Train:
+27292/31444 = 0.8679 in glove with 14699 unique values
+4152/31444 = 0.1321 not in glove with 3494 unique values 
+
+Dev:
+4013/4593 = 0.8737 in glove with 2124 unique values
+580/4593 = 0.1263 not in glove with 486 unique values
+
+Test:
+3893/4529 = 0.8596 in glove with 2157 unique values
+636/4529 = 0.1404 not in glove with 539 unique values
+
+For words that were not found in FastText, 0 vector of same size was utilized 300 Dimensions
+
+Train:
+30781/31444 = 0.9789 in fasttext with 17598 unique values 
+663/31444 = 0.0211 in fasttext with 595 unique values
+
+Dev:
+4502/4593 = 0.9802 in glove with 2529 unique values
+91/4593 = 0.0198 in glove with 81 unique values
+
+Test:
+4433/4529 = 0.9788 in glove with 2608 unique values
+96/4529 = 0.0212 in glove with 88 unique values
+
---
+++ preprocess/extract_fasttext.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import helper
+import config
+import numpy as np
+import os
+from zipfile import ZipFile
+import urllib.request
+import io
+
+
+
+# glove.6B.zip
+def download_fasttext_model(download_url, filename="crawl-300d-2M.vec.zip"):
+    """ Download and extract data """
+    
+    downloaded_filename = os.path.join('.', filename)
+    if not os.path.exists(downloaded_filename):
+        print ('Downloading fasttext model...')
+        urllib.request.urlretrieve(download_url,downloaded_filename)
+    else:
+        print('FastText model already downloaded...')
+    if not os.path.exists('crawl-300d-2M.vec'):
+        print ('Extracting fasttext data')
+        zipfile=ZipFile(downloaded_filename)
+        zipfile.extractall('./')
+        zipfile.close()
+    else:
+        print('FastText data already extracted...')
+
+'''
+Code to load FastText vectors in a dictionary obtained from FastText Site with some modification
+'''
+def load_vectors(fname):
+    fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+    n, d = map(int, fin.readline().split())
+    data = {}
+    for line in fin:
+        tokens = line.rstrip().split(' ')
+        for i in range(1,len(tokens)):
+            tokens[i] = float(tokens[i])
+        data[tokens[0]] = tokens[1:]
+    return data
+
+def extract_corresponding_fasttext_embeddings(pkl_data, data_dict, output_filepath):
+    print("------------------- Extracting FastText Embeddings -------------------")
+    zero_vec_300D = np.zeros(300,dtype = 'float32')
+    # word_fasttext_dict = dict()
+    words_in_fasttext = 0
+    words_not_in_fasttext = 0
+    in_fasttext_set = set()
+    not_in_fasttext_set = set()
+    total = len(pkl_data)
+
+
+    for dct in pkl_data:
+        word = dct['word']
+        if word in data_dict:
+            # word_fasttext_dict[word] = data_dict[word]
+            dct['fasttext'] = data_dict[word]
+            words_in_fasttext += 1
+            in_fasttext_set.add(word)
+        else:
+            # word_fasttext_dict[word] = zero_vec_300D
+            dct['fasttext'] = zero_vec_300D
+            words_not_in_fasttext += 1
+            not_in_fasttext_set.add(word)
+    
+    print(words_in_fasttext, "corresponding words found in FastText out of", total, "words in our set:",     words_not_in_fasttext*100/total, "percent and found", len(in_fasttext_set), "unique words")
+    print(words_not_in_fasttext, "corresponding words found in FastText out of", total, "words in our set:", words_not_in_fasttext*100/total, "percent and found", len(not_in_fasttext_set), "unique words")
+
+    
+    helper.write_pickle_file(pkl_data,output_filepath)
+
+
+
+if __name__ == "__main__":
+    train_pkl = helper.read_pickle_file(config.proc_paths['train'])
+    dev_pkl = helper.read_pickle_file(config.proc_paths['dev'])
+    test_pkl = helper.read_pickle_file(config.proc_paths['test'])
+    
+    filename = "crawl-300d-2M.vec.zip"
+    fasttext_url = 'https://dl.fbaipublicfiles.com/fasttext/vectors-english/crawl-300d-2M.vec.zip'
+
+    vector_file = "crawl-300d-2M.vec"
+    
+    download_fasttext_model(download_url=fasttext_url, filename=filename)
+    data_dict = load_vectors(vector_file)
+
+    train_outfile = "./../data/train_fasttext.pkl"
+    dev_outfile = "./../data/dev_fasttext.pkl"
+    test_outfile = "./../data/test_fasttext.pkl"
+
+    extract_corresponding_fasttext_embeddings(pkl_data = train_pkl,data_dict=data_dict,output_filepath = train_outfile)
+    extract_corresponding_fasttext_embeddings(pkl_data = dev_pkl,data_dict=data_dict,output_filepath = dev_outfile)
+    extract_corresponding_fasttext_embeddings(pkl_data = test_pkl,data_dict=data_dict,output_filepath = test_outfile)
